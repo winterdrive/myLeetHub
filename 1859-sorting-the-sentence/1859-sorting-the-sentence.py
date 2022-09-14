9329f0b72cb4dbc15cb1fa26dd3f60abf3adba53
winterdrive <90021888+winterdrive@users.noreply.github.com>
2022-09-14 14:21:36 +0800

    Time: 65 ms (8.10%), Space: 13.9 MB (56.70%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.py
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -1,9 +1,8 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
         result=''
-        myList=s.split(' ')
         myDict=dict()
-        for i in myList:
+        for i in s.split(' '):
             myDict[i[-1]+i[:-1]]=''
         for value in sorted(myDict):
             result=result+value[1:]+" "

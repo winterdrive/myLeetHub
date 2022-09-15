926036c6be1a9ebe4eb8fd6032c7d9a2c56e6254
winterdrive <90021888+winterdrive@users.noreply.github.com>
2022-09-15 16:14:28 +0800

    Time: 139 ms (78.69%), Space: 21.8 MB (33.04%) - LeetHub

--- 89-gray-code/89-gray-code.py
+++ 89-gray-code/89-gray-code.py
@@ -1,16 +1,14 @@
 class Solution:
-    def grayCode(self, n: int) -> List[int]:
+    def grayCode(self, n: int) -> list[int]:
         myList=list()
         myList.append('0')
-        myList.append('1')
-        currentLength=len(myList)
+        myList.append('1') #初始list建立
         for i in range(2,n+1):
-            reverseMyLsit=myList[::-1]
+            reverseMyLsit=myList[::-1] #建立鏡像list
             for j in range(0,len(myList)):    
-                myList[j]='0'+myList[j]
-                reverseMyLsit[j]='1'+reverseMyLsit[j]
-            myList=myList+reverseMyLsit
-        result=list()
+                myList[j]='0'+myList[j] #原本的list加上'0'
+                reverseMyLsit[j]='1'+reverseMyLsit[j] #鏡像的list加上'1'
+            myList=myList+reverseMyLsit #鏡像跟原本的加起來，進入下一個迴圈
         for k in range(0, len(myList)):
-            result.append(int(myList[k],2))
-        return result
+            myList[k]=(int(myList[k],2))
+        return myList

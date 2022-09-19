af2d6dc42aa8b613e5cb9174c254704870495345
winterdrive <90021888+winterdrive@users.noreply.github.com>
2022-09-19 15:07:38 +0800

    Time: 60 ms (39.41%), Space: 14 MB (92.67%) - LeetHub

--- 541-reverse-string-ii/541-reverse-string-ii.py
+++ 541-reverse-string-ii/541-reverse-string-ii.py
@@ -1,7 +1,5 @@
 class Solution:
     def reverseStr(self, s: str, k: int) -> str:
-        result=""
-        temp=""
         count=0
         for i in range(0,len(s),k):
             if(count%2==0):
@@ -9,5 +7,4 @@ class Solution:
                 count+=1
             else:
                 count+=1
-        result=s
-        return result
+        return s

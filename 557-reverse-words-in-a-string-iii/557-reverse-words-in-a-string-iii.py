85bd5cb038f52807365e1ef42f5e9db3c2b609aa
winterdrive <90021888+winterdrive@users.noreply.github.com>
2022-09-19 13:33:23 +0800

    Time: 38 ms (93.29%), Space: 14.7 MB (45.85%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.py
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.py
@@ -1,14 +1,20 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
+        # result=""
+        # temp=""
+        # for i in range(0,len(s)):
+        #     if s[i]!=' ' and i!=len(s)-1:
+        #         temp+=s[i]
+        #     elif i==len(s)-1:
+        #         temp+=s[i]
+        #         result=result+temp[::-1]
+        #     else:
+        #         result=result+temp[::-1]+" "
+        #         temp=""
+        # return result
+        
         result=""
-        temp=""
-        for i in range(0,len(s)):
-            if s[i]!=' ' and i!=len(s)-1:
-                temp+=s[i]
-            elif i==len(s)-1:
-                temp+=s[i]
-                result=result+temp[::-1]
-            else:
-                result=result+temp[::-1]+" "
-                temp=""
-        return result
+        myLsit=s.split(" ")
+        for i in myLsit:
+            result+=i[::-1]+" "
+        return result[:-1]

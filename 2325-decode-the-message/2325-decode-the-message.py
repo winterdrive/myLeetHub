8903bea17a930553ef109de5cb528356206a16a0
winterdrive <90021888+winterdrive@users.noreply.github.com>
2022-09-21 13:56:59 +0800

    Time: 71 ms (21.45%), Space: 13.9 MB (40.61%) - LeetHub

--- 2325-decode-the-message/2325-decode-the-message.py
+++ 2325-decode-the-message/2325-decode-the-message.py
@@ -3,18 +3,14 @@ class Solution:
         result=""
         alpha='abcdefghijklmnopqrstuvwxyz'
         myDict=dict()
+        myDict[" "]=" "
         for i in range(0,len(key)):
-            if key[i]==" ":
-                continue
-            if len(myDict.keys())==26:
-                break
             if key[i] not in myDict:
                 myDict[key[i]]=alpha[0]
                 alpha=alpha[1:]
+                if(len(alpha)==0):
+                    break
         # print(myDict)        
         for i in range(0,len(message)):
-            if(message[i]==" "):
-                result+=" "
-            else:
-                result+=myDict[message[i]]
+            result+=myDict[message[i]]
         return result

d5edddf180163cf0df7455762e4660afb8c6fba6
winterdrive <90021888+winterdrive@users.noreply.github.com>
2022-09-22 13:57:32 +0800

    Time: 52 ms (52.36%), Space: 13.9 MB (11.30%) - LeetHub

--- 2129-capitalize-the-title/2129-capitalize-the-title.py
+++ 2129-capitalize-the-title/2129-capitalize-the-title.py
@@ -1,11 +1,9 @@
 class Solution:
     def capitalizeTitle(self, title: str) -> str:
-        result=""
         myList=title.lower().split(" ")
         for i in range(0,len(myList)):
             if len(myList[i])>2:
                 myList[i]=myList[i].capitalize()
             else:
                 myList[i]=myList[i]
-                
         return " ".join(myList)

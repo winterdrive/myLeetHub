d3a77e2c90a619469879601acb3025b3e4f2992a
winterdrive <90021888+winterdrive@users.noreply.github.com>
2022-09-22 13:33:45 +0800

    Time: 34 ms (88.86%), Space: 13.8 MB (49.41%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.py
+++ 709-to-lower-case/709-to-lower-case.py
@@ -6,4 +6,3 @@ class Solution:
             s=s.replace('A','a').replace('B','b').replace('C','c').replace('D','d').replace('E','e').replace('F','f').replace('G','g').replace('H','h').replace('I','i').replace('J','j').replace('K','k').replace('L','l').replace('M','m').replace('N','n').replace('O','o').replace('P','p').replace('Q','q').replace('R','r').replace('S','s').replace('T','t').replace('U','u').replace('V','v').replace('W','w').replace('X','x').replace('Y','y').replace('Z','z')
         return s
 
-        return s

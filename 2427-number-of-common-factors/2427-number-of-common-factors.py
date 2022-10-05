893752e4bb216d57ee241b3ebc01e3cd2f9c9cf7
winterdrive <90021888+winterdrive@users.noreply.github.com>
2022-10-05 22:34:42 +0800

    Time: 40 ms (79.21%), Space: 13.8 MB (63.74%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.py
+++ 2427-number-of-common-factors/2427-number-of-common-factors.py
@@ -1,8 +1,8 @@
 class Solution:
     def commonFactors(self, a: int, b: int) -> int:
         result=0
-        myMax=max(a,b)
-        for i in range(1,myMax+1):
+        myMin=min(a,b)
+        for i in range(1,myMin+1):
             if a%i==0 and b%i==0:
                 result+=1
         return result

efeaa8cf071405b99060fab8a836915028555865
winterdrive <90021888+winterdrive@users.noreply.github.com>
2022-11-16 18:24:14 +0800

    Time: 152 ms (68.48%), Space: 13.8 MB (65.43%) - LeetHub

---
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countOperations(self, num1: int, num2: int) -> int:
+        result=0
+        while num1!=0 and num2!=0:
+            if num1>=num2:
+                num1-=num2
+                result+=1
+            else:
+                num2-=num1
+                result+=1
+        return result

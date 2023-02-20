66e5880d066b7909f34830a18593003cbf866465
winterdrive <90021888+winterdrive@users.noreply.github.com>
2023-02-20 14:47:05 +0800

    Time: 24 ms (58.66%), Space: 42.8 MB (83.21%) - LeetHub

--- 0524-longest-word-in-dictionary-through-deleting/0524-longest-word-in-dictionary-through-deleting.java
+++ 0524-longest-word-in-dictionary-through-deleting/0524-longest-word-in-dictionary-through-deleting.java
@@ -4,15 +4,15 @@ class Solution {
         for (int i = 0; i<dictionary.size(); i++) {
             String word=dictionary.get(i);
             if (isSubString(word, s)) {
-                if ((word.length() == tempWord.length() && word.compareTo(tempWord) < 0)
-                        || word.length() > tempWord.length()) {
+                if ((word.length() == tempWord.length() && word.compareTo(tempWord) < 0)) {
+                    tempWord = word;
+                }else if(word.length() > tempWord.length()){
                     tempWord = word;
                 }
             }
         }
         return tempWord;
     }
-    
     
     private static boolean isSubString(String word, String s) {
         int i = 0, j = 0;
@@ -24,5 +24,4 @@ class Solution {
         }
         return i == word.length();
     }
-    
 }
